24b15895bd608cefa8f8710ef50f1d2aca63bc1c
Toma Vedishcheva <toma-vesta@mail.ru>
2021-12-06 16:59:46 +0000

    Lambda

--- src/Simple.java
+++ src/Simple.java
@@ -4,6 +4,7 @@ import helpers.Greeting;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Simple {
     public static void main(String[] args) {
@@ -25,6 +26,14 @@ public class Simple {
         sort(myStringArray);
 
         printer.print(myStringArray);
+
+        Stream
+                .of(1, 2, 3, 2, 1)
+                .map(s -> s * s)
+                .distinct()
+                .forEach(System.out::println);
+
+        myArray.stream().mapToInt(a->a).min().ifPresent(System.out::println);
 
         greeting.sayBye();
     }
