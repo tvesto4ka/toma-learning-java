74e8a37808e6533733173df2995805606c2ea601
Toma Vedishcheva <toma-vesta@mail.ru>
2021-12-10 13:41:19 +0000

    EPAM interview add Tokenizer

--- src/EPAM/interview/AcronymHelper.java
+++ src/EPAM/interview/AcronymHelper.java
@@ -1,6 +1,10 @@
 package EPAM.interview;
 
+import java.util.StringTokenizer;
+
 public class AcronymHelper {
+    public static final String message = "Hi my      name is Toma";
+
     public static String getAcronym(String input) {
         String[] words = input.split(" ");
         StringBuilder result = new StringBuilder();
@@ -12,7 +16,20 @@ public class AcronymHelper {
         return result.toString().toUpperCase();
     }
 
+    public static String getAcronymWithTokenizer(String input) {
+        StringBuilder result = new StringBuilder();
+        StringTokenizer tokenizer = new StringTokenizer(input, " ");
+        while (tokenizer.hasMoreTokens()) {
+            String word = tokenizer.nextToken();
+            if (word.length() > 0) {
+                result.append(word.charAt(0));
+            }
+        }
+        return result.toString().toUpperCase();
+    }
+
     public static void main(String[] args) {
-        System.out.println(getAcronym("Hi my      name is Toma"));
+        System.out.println(getAcronym(message));
+        System.out.println(getAcronymWithTokenizer(message));
     }
 }
