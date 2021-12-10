af1a963408f0519ab30e9ab525d938b2830554cd
Toma Vedishcheva <toma-vesta@mail.ru>
2021-12-10 06:47:04 +0000

    change main name

--- src/Starter.java
+++ src/Starter.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Simple {
+public class Starter {
     public static void main(String[] args) {
 
         Greeting greeting = new Greeting();
