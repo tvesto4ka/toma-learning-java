92bb6b3b147f84c4389c141f305e58e3402c9aea
tamaratisova <toma-vesta@mail.ru>
2024-08-13 11:44:31 +0500

    length of last word with split string

--- src/basics/LengthOfLastWord.java
+++ src/basics/LengthOfLastWord.java
@@ -1,19 +1,20 @@
 package basics;
 
-import java.util.StringTokenizer;
-
 public class LengthOfLastWord {
     public static int lengthOfLastWord(String s) {
-        StringTokenizer stringTokenizer = new StringTokenizer(s, " ");
-        String str = "";
-        while (stringTokenizer.hasMoreTokens()) {
-            str = stringTokenizer.nextToken();
+        String[] tokens = s.split(" ");
+        String lastWord = "";
+        for (int i = tokens.length - 1; i >= 0; i--) {
+            if (tokens[i].length() > 0) {
+                lastWord = tokens[i];
+                break;
+            }
         }
-        return str.length();
+        return lastWord.length();
     }
 
     public static void main(String[] args) {
-        String s1 = "luffy is still joyboy";
+        String s1 = "   fly me   to   the moon  ";
         System.out.println(lengthOfLastWord(s1));
     }
 }
